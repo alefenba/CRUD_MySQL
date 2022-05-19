efdf1c00ce9feb4828b020f1cc62d64ea81a2bf4
alefenba <alefe.182@outlook.com>
2022-05-19 20:54:31 -0300

    Criada função de criar e remover tabela #2

--- inicio.py
+++ inicio.py
@@ -6,5 +6,25 @@ con = pymysql.connect(
     user="root",
     password=config("password"),
     port=3306,
+    db="aulapythonfull",
     charset="utf8mb4",
     cursorclass = pymysql.cursors.DictCursor)
+
+def criaTabela(nomeTabela):
+    try:
+        with con.cursor() as cursor:
+            cursor.execute(f"create table {nomeTabela}(nome varchar(50))")
+        print("Tabela criada com sucesso")
+    except Exception as e:
+        print(f'Ocorreu um erro {e}')
+
+
+def removeTabela(nomeTabela):
+    try:
+        with con.cursor() as cursor:
+            cursor.execute(f"drop table {nomeTabela}")
+        print("Tabela excluida com sucesso")
+    except Exception as e:
+        print(f'Ocorreu um erro {e}')
+removeTabela("teste2")
+con.close()
