8bdcd0c478e203f854c5cf716ef731696b7c704d
alefenba <alefe.182@outlook.com>
2022-05-20 20:36:11 -0300

    Introdução ao ORM e SQLAlchemy #7

---
+++ ORM.py
@@ -0,0 +1,26 @@
+from decouple import config
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+USUARIO = "root"
+SENHA = config("password")
+HOST = "127.0.0.1"
+BANCO = "aulapythonfull"
+PORT = "3306"
+
+
+CONN = f"mysql+pymysql://{USUARIO}:{SENHA}@{HOST}:{PORT}/{BANCO}"
+engine = create_engine(CONN, echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class Pessoa(Base):
+    __tablename__ = "Pessoa"
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(50))
+    usuario = Column(String(20))
+    senha = Column(String(10))
+
+Base.metadata.create_all(engine)
