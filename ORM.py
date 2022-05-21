04d22e6aeb8150e2acef6369a2ed8695f2b725ac
alefenba <alefe.182@outlook.com>
2022-05-20 22:14:07 -0300

    Utilizando chave estrangeira #14

--- ORM.py
+++ ORM.py
@@ -1,5 +1,6 @@
+from typing import Collection
 from decouple import config
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -17,10 +18,25 @@ session = Session()
 Base = declarative_base()
 
 class Pessoa(Base):
-    __tablename__ = "Pessoa"
+    __tablename__ = "pessoa"
     id = Column(Integer, primary_key=True)
     nome = Column(String(50))
     usuario = Column(String(20))
     senha = Column(String(10))
+
+
+class Categoria(Base):
+    __tablename__ = "categoria"
+    id = Column(Integer,primary_key=True)
+    categoria = Column(String(50))
+
+
+class Produto(Base):
+    __tablename__ = "produto"
+    id = Column(Integer, primary_key=True)
+    produto = Column(String(50))
+    idCategoria = Column(Integer, ForeignKey('categoria.id'))
+
+
 
 Base.metadata.create_all(engine)
